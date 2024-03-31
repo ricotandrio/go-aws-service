52faf129ae624c458e198254cd05e2ddf0bd6b75
ricotandrio@gmail.com <ricotandrio@gmail.com>
2024-03-31 11:08:29 +0700

    fix: print message at end point

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"go-service-aws/configs"
 	"go-service-aws/routers"
-	"fmt"
 )
 
 func main() {
@@ -13,10 +13,10 @@ func main() {
 	router := routers.SetupRouter()
 
 	err := router.Run(":8080")
-	
+
 	if err != nil {
 		panic(err)
 	}
 	
-	fmt.Println("Server is running on http://localhost:8080")
+	fmt.Println("Server is running on port 8080")
 }
